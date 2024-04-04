a44c252d6f0f44a18780ec0b9ff206a43579fe06
saadweb <saad.zahir@staffone.fr>
2024-04-04 11:55:48 +0000

    add download option

--- javascript/telecharger.js
+++ javascript/telecharger.js
@@ -1,35 +1,17 @@
-var btn = document.getElementById('generatePDF');
+var btn = document.querySelector('#generatePDF');
+var inputIcons = document.querySelector('.input-icons');
 
-console.log(btn);
-
-btn.addEventListener('onClick', printDiv('main'),false);
+btn.addEventListener('click',()=>printDiv('cv_body'),false);
 
 function printDiv(divName) {
-    console.log('clicked');
+    inputIcons.style.display = 'none';
     var printContents = document.getElementById(divName).innerHTML;
     var originalContents = document.body.innerHTML;
-
     document.body.innerHTML = printContents;
-
     window.print();
-
     document.body.innerHTML = originalContents;
 }
-// $(document).ready(function(){
 
-// var doc = new jsPDF();
-// var specialElementHandlers = {
-//     '#editor': function (element, renderer) {
-//         return true;
-//     }
-// };
-// //margins.left, // x coord   margins.top, { // y coord
-// $('.generatePDF').click(function () {
-//     console.log('clicked');
-//     doc.fromHTML($('.main').html(), 15, 15, {
-//         'width': 700,
-//         'elementHandlers': specialElementHandlers
-//     });
-//     doc.save('sample_file.pdf');
-// });
-// });
+window.onafterprint = async(event) => {
+    location.reload();
+  };
